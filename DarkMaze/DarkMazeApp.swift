0c0a4f4412a04bd7e14bfa3389f2e288aa3ba80f
slavny11 <slavny45@gmail.com>
2023-04-28 10:36:38 +0200

    Initial Commit

---
+++ DarkMaze/DarkMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkMazeApp.swift
+//  DarkMaze
+//
+//  Created by Viacheslav on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DarkMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
