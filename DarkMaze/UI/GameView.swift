01df105e39f3307f72ddca3fefd1301b6e803bd7
slavny11 <slavny45@gmail.com>
2023-05-26 12:56:23 +0200

    Add UI to buttons

--- DarkMaze/UI/GameView.swift
+++ DarkMaze/UI/GameView.swift
@@ -22,18 +22,31 @@ struct GameView: View {
 
     var body: some View {
         VStack {
-            HStack {
+            Text("LEVEL 1")
+                .foregroundColor(.white)
+                .font(.system(size: 32, weight: .black, design: .monospaced))
+            HStack(spacing: 25) {
                 Button {
                     //reset game
                 } label: {
                     Text("Reset")
+                        .frame(width: 150, height: 50)
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .font(.system(size: 24, weight: .black, design: .monospaced))
                 }
                 Button {
                     state = .main
                 } label: {
                     Text("Main")
+                        .frame(width: 150, height: 50)
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .font(.system(size: 24, weight: .black, design: .monospaced))
+                        .border(.white, width: 4)
                 }
             }
+            .padding(.bottom, 50)
 
             SpriteView(scene: scene, transition: nil)
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
